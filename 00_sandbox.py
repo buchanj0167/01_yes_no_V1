aafd4e7b0cc40bf684b7fa1b76a3a0d23f6a2755
buchanj0167 <99302434+buchanj0167@users.noreply.github.com>
2022-02-18 12:02:18 +1300

    Add files via upload

---
+++ 00_sandbox.py
@@ -0,0 +1,14 @@
+print("hello world")
+get_name = input ("who are you")
+print()
+print("hello" , get_name)
+
+
+get_number = int(input("choose a number?"))
+times_five = get_number * 5
+print(times_five)
+
+
+get_number = int(input("choose a number?"))
+times_Ten = get_number * 10
+print(times_Ten)
