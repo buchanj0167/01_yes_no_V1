a78b11abae109c1fa564c078423398383afc538a
buchanj0167 <99302434+buchanj0167@users.noreply.github.com>
2022-02-23 14:13:36 +1300

    Add files via upload

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -14,10 +14,35 @@ elif show_instructions == "no":
     print("display instructions")
 
 elif show_instructions == "n":
-    print("display instructions")
+    print("**** How to play ****")
+    print()
+    print("The rules of the game go here")
+
 
 #If they say no, output 'display instructions'
 else:
      print("Please answer yes / no")
 
+
+# Main routine go here
+
+error = "Please enter an whole number between 1 and 10\n"
+
+valid = False
+while not valid:
+        try:
+            #ask the question
+            response = int(input("How much would you"
+                                "like to play with? "))
+            #if the amount is too low / too high give
+            if 0 < response <= 10:
+                print("You have asked to play"
+                    "with ${}".format(response))
+
+            # output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
 
